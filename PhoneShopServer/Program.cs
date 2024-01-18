35fcd741d97bcb79a42d6dc0302053d5bde2baa0
softvasco <sprvasco@hotmail.com>
2024-01-18 17:15:08 +0000

    other inicial steps

--- PhoneShopServer/Program.cs
+++ PhoneShopServer/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,12 +15,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
-
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
-
+app.MapFallbackToFile("index.html");
 app.Run();
---
+++ PhoneShopSharedLibrary/Models/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneShopSharedLibrary.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required, Range(0.1, 9999.99)]
+        public decimal Price { get; set; }
+        [Required, DisplayName("Product Image")]
+        public string? Base64Img { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateUploaded { get; set; } = DateTime.Now;
+    }
+}
